7d7bfc272b981346f7c4738d803bd678d846e14f
reza goldouzian <reza.goldouzian@cern.ch>
2021-12-21 09:55:27 -0500

    first commit

---
+++ cards/models/bnv-Reza/coupling_orders.py
@@ -0,0 +1,20 @@
+# This file was automatically created by FeynRules $Revision: 845 $
+# Mathematica version: 8.0 for Linux x86 (64-bit) (November 7, 2010)
+# Date: Tue 7 Feb 2012 13:57:30
+
+
+from object_library import all_orders, CouplingOrder
+
+
+BNV = CouplingOrder(name = 'BNV',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
